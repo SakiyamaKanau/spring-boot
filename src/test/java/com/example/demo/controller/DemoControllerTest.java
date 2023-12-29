d59e181dd269dde368f6414631ec0e666b8c60e1
崎山　衷 <kanaupc0708@gmail.com>
2023-12-29 17:35:25 +0900

    create preMain

---
+++ src/test/java/com/example/demo/controller/DemoControllerTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@WebMvcTest(DemoController.class)
+public class DemoControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testIndex() throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.view().name("index"))
+                .andExpect(MockMvcResultMatchers.model().attributeExists("message"))
+                .andExpect(MockMvcResultMatchers.model().attribute("message", "Hello, Spring Boot!"));
+    }
+}
